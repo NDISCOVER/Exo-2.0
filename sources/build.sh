b90b390895ebd5906ecc07489c91d5ba4b15df7d
CJ Dunn <cjdunn@Lappy-447.local>
2019-11-06 10:17:45 -0500

    temp comment out

--- sources/build.sh
+++ sources/build.sh
@@ -2,65 +2,65 @@
 set -e
 
 
-echo "Generating Static fonts"
-mkdir -p ../fonts
-fontmake -m Exo_Pro.designspace -i -o ttf --output-dir ../fonts/ttf/
-fontmake -m Exo_Pro.designspace -i -o otf --output-dir ../fonts/otf/
-fontmake -m Exo_Pro-Italic.designspace -o ttf --output-dir ../fonts/ttf/
-fontmake -m Exo_Pro-Italic.designspace -o otf --output-dir ../fonts/otf/
+# echo "Generating Static fonts"
+# mkdir -p ../fonts
+# fontmake -m Exo_Pro.designspace -i -o ttf --output-dir ../fonts/ttf/
+# fontmake -m Exo_Pro.designspace -i -o otf --output-dir ../fonts/otf/
+# fontmake -m Exo_Pro-Italic.designspace -o ttf --output-dir ../fonts/ttf/
+# fontmake -m Exo_Pro-Italic.designspace -o otf --output-dir ../fonts/otf/
 
 echo "Generating VFs"
-mkdir -p ../fonts/vf
+# mkdir -p ../fonts/vf
 fontmake -m Exo_Pro.designspace -o variable --output-path ../fonts/vf/Exo_Pro[wght].ttf
-fontmake -m Exo_Pro-Italic.designspace -o variable --output-path ../fonts/vf/Exo_Pro-Italic[wght].ttf
+# fontmake -m Exo_Pro-Italic.designspace -o variable --output-path ../fonts/vf/Exo_Pro-Italic[wght].ttf
 
 rm -rf master_ufo/ instance_ufo/ instance_ufos/*
 
 
-echo "Post processing"
-ttfs=$(ls ../fonts/ttf/*.ttf)
-for ttf in $ttfs
-do
-	gftools fix-dsig -f $ttf;
-	ttfautohint $ttf "$ttf.fix";
-	mv "$ttf.fix" $ttf;
-done
+# echo "Post processing"
+# ttfs=$(ls ../fonts/ttf/*.ttf)
+# for ttf in $ttfs
+# do
+# 	gftools fix-dsig -f $ttf;
+# 	ttfautohint $ttf "$ttf.fix";
+# 	mv "$ttf.fix" $ttf;
+# done
 
-vfs=$(ls ../fonts/vf/*\[wght\].ttf)
+# vfs=$(ls ../fonts/vf/*\[wght\].ttf)
 
-echo "Post processing VFs"
-for vf in $vfs
-do
-	gftools fix-dsig -f $vf;
-	ttfautohint-vf --stem-width-mode nnn $vf "$vf.fix";
-	mv "$vf.fix" $vf;
-done
+# echo "Post processing VFs"
+# for vf in $vfs
+# do
+# 	gftools fix-dsig -f $vf;
+# 	ttfautohint-vf --stem-width-mode nnn $vf "$vf.fix";
+# 	mv "$vf.fix" $vf;
+# done
 
 
 
-echo "Fixing VF Meta"
-gftools fix-vf-meta $vfs;
+# echo "Fixing VF Meta"
+# gftools fix-vf-meta $vfs;
 
-echo "Dropping MVAR"
-for vf in $vfs
-do
-	mv "$vf.fix" $vf;
-	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
-	rtrip=$(basename -s .ttf $vf)
-	new_file=../fonts/vf/$rtrip.ttx;
-	rm $vf;
-	ttx $new_file
-	rm $new_file
-done
+# echo "Dropping MVAR"
+# for vf in $vfs
+# do
+# 	mv "$vf.fix" $vf;
+# 	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
+# 	rtrip=$(basename -s .ttf $vf)
+# 	new_file=../fonts/vf/$rtrip.ttx;
+# 	rm $vf;
+# 	ttx $new_file
+# 	rm $new_file
+# done
 
-echo "Fixing Hinting"
-for vf in $vfs
-do
-	gftools fix-hinting $vf;
-	mv "$vf.fix" $vf;
-done
-for ttf in $ttfs
-do
-	gftools fix-hinting $ttf;
-	mv "$ttf.fix" $ttf;
-done
+# echo "Fixing Hinting"
+# for vf in $vfs
+# do
+# 	gftools fix-hinting $vf;
+# 	mv "$vf.fix" $vf;
+# done
+# for ttf in $ttfs
+# do
+# 	gftools fix-hinting $ttf;
+# 	mv "$ttf.fix" $ttf;
+# done
