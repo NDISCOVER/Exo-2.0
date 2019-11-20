f2f5b6a85e43a706f0bf9b16ae153883f29de0ef
CJ Dunn <cjdunn@Lappy-447.local>
2019-11-20 15:08:24 -0500

    add report script

---
+++ sources/report.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+
+gftools qa -f ../fonts/vf/Exo2[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_roman
+
+# gftools qa -f ../fonts/vf/Exo2-Italic[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_italic
